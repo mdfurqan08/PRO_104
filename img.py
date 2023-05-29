295bf0cac17cc273808bbb344cfd6b56afe85e99
Mohammed Furqan <84570810+mdfurqan08@users.noreply.github.com>
2023-05-29 13:40:07 +0530

    Update img.py

--- img.py
+++ img.py
@@ -9,6 +9,64 @@ cv2.putText(img,
             0.5,
             (255,255,255))
 
+cv2.putText(img,
+            "mercury",
+            (120,250),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,205,255))
+
+cv2.putText(img,
+            "venus",
+            (200,150),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,205,255))
+
+cv2.putText(img,
+            "earth",
+            (300,150),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,205,255))
+
+cv2.putText(img,
+            "mars",
+            (400,150),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,205,255))
+
+cv2.putText(img,
+            "jupiter",
+            (500,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            1.0,
+            (255,205,255))
+
+cv2.putText(img,
+            "saturn",
+            (700,100),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,205,255))
+
+cv2.putText(img,
+            "uranus",
+            (950,100),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255))
+
+cv2.putText(img,
+            "meacuery",
+            (1100,100),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,205,255))
+
+
+
 cv2.imshow("hlo",img)
 
 cv2.waitKey(0)
